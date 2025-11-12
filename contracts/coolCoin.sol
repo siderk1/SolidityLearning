313183615316359630b455709f7733641549d926
siderk1 <nick.kr1803@gmail.com>
2025-11-12 06:05:57 +0200

    feat(CoolCoin Economics): Reworked internal transfer method, added buy and sell functions

--- contracts/coolCoin.sol
+++ contracts/coolCoin.sol
@@ -2,21 +2,48 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract CoolCoin is IERC20 {
-
+contract CoolCoin is IERC20, Ownable, ReentrancyGuard {
+    uint256 public timeToVote;
+    bool public isVotingInProgress;
+    uint256 public currentPrice;
+    uint256 private _feeBps;
+    uint256 public constant BPS_DENOMINATOR = 10_000;
+    uint256 public constant MAX_FEE_BPS = 1000;
+    
     uint256 private _totalSupply;
     string private _name = "CoolCoin";
     string private _symbol = "COOL";
     uint8 private _decimals = 18;
-
+    
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    constructor (string memory name_, string memory symbol_, uint256 initialSupply) {
+    event FeeUpdated(uint256 currentFeeBps, uint256 newFeeBps);
+    event Buy(address buyer, uint256 ethIn, uint256 tokensOut, uint256 feeTokens, uint256 price);
+    event Sell(address seller, uint256 tokensIn, uint256 ethOut, uint256 feeTokens, uint256 price);
+    event FeeBurned(uint256 amount, uint256 timestamp);
+
+
+    modifier notDuringVotingIfVoted () {
+        _;
+    }
+    constructor (
+        string memory name_,
+        string memory symbol_,
+        uint256 timeToVote_,
+        uint256 currentPrice_,
+        uint256 feeBps_
+    ) 
+        Ownable(msg.sender) 
+    {
         _name = name_;
         _symbol = symbol_;
-        _mint(msg.sender, initialSupply);
+        timeToVote = timeToVote_;
+        currentPrice = currentPrice_;
+        _feeBps = feeBps_;
     }
 
     function name() public view returns (string memory) {
@@ -31,6 +58,10 @@ contract CoolCoin is IERC20 {
         return _decimals;
     }
 
+    function feeBps() external view returns (uint256) {
+        return _feeBps;
+    }
+
     function totalSupply() external view override returns (uint256) {
         return _totalSupply;
     }
@@ -39,20 +70,9 @@ contract CoolCoin is IERC20 {
         return _balances[account];
     }
 
-    function _transfer(address to, uint256 amount) internal virtual returns (bool) {
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-
-        unchecked {
-            _balances[msg.sender] -= amount;
-            _balances[to] += amount;
-        }
-
-        emit Transfer(msg.sender, to, amount);
+    function transfer(address to, uint256 amount) external override notDuringVotingIfVoted returns (bool) {
+        _transfer(msg.sender, to, amount);
         return true;
-    }
-
-    function transfer(address to, uint256 amount) external override returns (bool) {
-        return _transfer(to, amount);
     }
 
     function allowance(address owner, address spender) external view override returns (uint256) {
@@ -71,7 +91,7 @@ contract CoolCoin is IERC20 {
     function transferFrom(address from, address to, uint256 amount) external override returns (bool) {
         require(_allowances[from][msg.sender] >= amount, "Insufficient allowance");
 
-        _transfer(to, amount);
+        _transfer(from, to, amount);
         unchecked {
             _allowances[from][msg.sender] -= amount;
         }
@@ -79,10 +99,77 @@ contract CoolCoin is IERC20 {
         return true;
     }
 
-     function _mint(address to, uint256 amount) internal virtual {
-        _totalSupply += amount;
-        _balances[to] += amount;
+    function setFeeBps(uint256 newFeeBps) external onlyOwner {
+        require(newFeeBps <= MAX_FEE_BPS, "Max fee is 10%");
+        emit FeeUpdated(_feeBps, newFeeBps);
+        _feeBps = newFeeBps;
+    }
 
-        emit Transfer(address(0), to, amount);
+    function _moveFunds(address from, address to, uint256 amount) internal {
+        if (from == address(0)){
+            _totalSupply += amount;
+        } else {
+            require(_balances[from] >= amount, "Insufficient balance");
+            unchecked {
+                _balances[from] -= amount;
+            }
+        }
+
+        if (to == address(0)){
+            unchecked {
+                _totalSupply -= amount;
+            }
+        } else {
+            unchecked {
+                _balances[to] += amount;
+            }
+        }
+        emit Transfer(from, to, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0) && to != address(0));
+        _moveFunds(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        _moveFunds(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+       _moveFunds(account, address(0), amount);
+    }
+
+    function buy() external payable notDuringVotingIfVoted {
+        require(msg.value > 0);
+        uint256 tokensGross = msg.value * (10 ** _decimals) / currentPrice;
+        uint256 fee = tokensGross * _feeBps / BPS_DENOMINATOR;
+        uint256 tokensNet = tokensGross - fee;
+        require(tokensNet > 0);
+        _mint(address(this), fee);
+        _mint(msg.sender, tokensNet);
+
+        emit Buy(msg.sender, msg.value, tokensNet, fee, currentPrice);
+    }
+
+    function sell(uint256 tokensAmount) external nonReentrant notDuringVotingIfVoted {
+        require(tokensAmount > 0, "Zero amount");
+        require(_balances[msg.sender] >= tokensAmount, "Insufficient balance");
+        require(currentPrice > 0, "Price not set");
+
+        uint256 fee = tokensAmount * _feeBps / BPS_DENOMINATOR;
+        uint256 tokensNet = tokensAmount - fee;
+
+        uint256 ethOut = tokensNet * currentPrice / (10 ** _decimals);
+        require(address(this).balance >= ethOut);
+        
+
+        _transfer(msg.sender, address(this), fee);
+        _burn(msg.sender, tokensNet);
+
+        (bool sent, ) = msg.sender.call{value: ethOut}("");
+        require(sent, "ETH transfer failed");
+
+        emit Sell(msg.sender, tokensAmount, ethOut, fee, currentPrice);
     }
 }
